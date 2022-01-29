c414c3abaa2ae0aff6bc95167c9ff0cf37f83b9e
SamDev-7 <66446458+SamDev-7@users.noreply.github.com>
2022-01-28 19:47:54 -0500

    Fixed Typos

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-# goldtrident
-Golden Trident mod 
+# Golden Trident Mod
 This mod aims to add a golden trident that lasts forever for the first player who gets a trident on the server.
---
+++ src/main/java/goldtrident/samdev/gq/GoldTrident.java
@@ -0,0 +1,10 @@
+package goldtrident.samdev.gq;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("goldtrident")
+public class GoldTrident
+{
+
+    public static final String MODID = "goldtrident";
+}
